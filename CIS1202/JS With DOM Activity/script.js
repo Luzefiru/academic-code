776ec74334a82189ac8707c2eb55284079b6a0ce
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-19 14:47:34 +0800

    feat: add text field for append button

--- CIS1202/JS With DOM Activity/script.js
+++ CIS1202/JS With DOM Activity/script.js
@@ -1,5 +1,5 @@
-btnShuffleImage = document.querySelector(".btn--shuffle-image")
-headerImage = document.querySelector('[alt="Banner Image"]')
+const btnShuffleImage = document.querySelector(".btn--shuffle-image")
+const headerImage = document.querySelector('[alt="Banner Image"]')
 
 imgNumber = 1 // number to determine which banner-x.jpg to use, where x is (imgNumber % 5 + 1)
 btnShuffleImage.addEventListener("click", (e) => {
