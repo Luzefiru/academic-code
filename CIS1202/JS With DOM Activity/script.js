581367d1e55d2c1f9cc79a315acf6a6f9a9d2df0
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-19 15:22:06 +0800

    docs: add documentation to functions

--- CIS1202/JS With DOM Activity/script.js
+++ CIS1202/JS With DOM Activity/script.js
@@ -5,12 +5,14 @@ const btnTextAppend = document.querySelector(".btn--text-append");
 const textArea = document.querySelector(".form-control");
 const appendArea = document.querySelector(".append-area");
 
+// shuffles the banner image forward by 1 image, max is until banner-5.jpg
 imgNumber = 1 // number to determine which banner-x.jpg to use, where x is (imgNumber % 5 + 1)
 btnShuffleImage.addEventListener("click", (e) => {
     imgNumber++; // increments to move to the next image
     headerImage.setAttribute('src', `./res/banner-${imgNumber % 5 + 1}.jpg`);
 })
 
+// textarea validation logic for handling the max of 1000 characters
 textArea.addEventListener('keyup', (e) => {
     if (textArea.value.length > 1000) {
         textArea.classList.add('is-invalid');
@@ -20,6 +22,7 @@ textArea.addEventListener('keyup', (e) => {
     }
 })
 
+// when you click the append button, set the appendArea's text to its current + whatever is in textArea
 btnTextAppend.addEventListener("click", (e) => {
     console.log(appendArea)
     appendArea.textContent = appendArea.textContent + ` ${textArea.value}`;
