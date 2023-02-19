f1268c5a0f4fd7105f1e45ec5dcfe77c0b0d09d3
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-19 15:20:21 +0800

    feat: add append text functionality

--- CIS1202/JS With DOM Activity/script.js
+++ CIS1202/JS With DOM Activity/script.js
@@ -1,6 +1,9 @@
 const btnShuffleImage = document.querySelector(".btn--shuffle-image");
 const headerImage = document.querySelector('[alt="Banner Image"]');
+
+const btnTextAppend = document.querySelector(".btn--text-append");
 const textArea = document.querySelector(".form-control");
+const appendArea = document.querySelector(".append-area");
 
 imgNumber = 1 // number to determine which banner-x.jpg to use, where x is (imgNumber % 5 + 1)
 btnShuffleImage.addEventListener("click", (e) => {
@@ -12,10 +15,12 @@ textArea.addEventListener('keyup', (e) => {
     if (textArea.value.length > 1000) {
         textArea.classList.add('is-invalid');
     }
-})
-
-textArea.addEventListener('keyup', (e) => {
-    if (textArea.value.length < 1001) {
+    else if (textArea.value.length < 1001) {
         textArea.classList.remove('is-invalid');
     }
+})
+
+btnTextAppend.addEventListener("click", (e) => {
+    console.log(appendArea)
+    appendArea.textContent = appendArea.textContent + ` ${textArea.value}`;
 })
