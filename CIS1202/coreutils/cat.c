577694104207b5d07e0953d007a14172a8cf1b5c
Christian de Jesus IV <105530193+Luzefiru@users.noreply.github.com>
2023-05-12 19:25:42 +0800

    fix: add exit() on NULL file pointer

--- CIS1202/coreutils/cat.c
+++ CIS1202/coreutils/cat.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
     // handles any file pointer errors
     if (fp == NULL) {
         fprintf(stderr, "cat: %s: No such file or directory", argv[1]);
+        exit(EXIT_FAILURE);
     }
 
     // happy path
