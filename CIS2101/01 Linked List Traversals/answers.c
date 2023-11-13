8853cfc1f6e98dc512c19faeb7783a16e0194d54
Luzefiru <ian.deejay.iv@gmail.com>
2023-11-13 18:56:29 +0800

    docs: add documentation for LL functions

--- CIS2101/01 Linked List Traversals/answers.c
+++ CIS2101/01 Linked List Traversals/answers.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 typedef struct node {
-  char elem;
+  char data;
   struct node *link;
 } ctype, *List;
 
@@ -11,7 +11,7 @@ void displayListPN(List L);
 void displayListPPN(List *L);
 void deleteNodeIndex(List *L, int targetIndex);
 void deleteNodeValue(List *L, char targetValue);
-void deleteAllOccurences(List *L, char targetValue);
+void deleteAllOccurrences(List *L, char targetValue);
 void updateNodeValue(List *L, char oldValue, char newValue);
 void insertFirst(List *L, char value);
 void insertLast(List *L, char value);
@@ -39,7 +39,7 @@ int main(void) {
   // 3)
   q = L;
 
-  printf("%c %c should be H H\n", (*p)->elem, q->elem);
+  printf("%c %c should be H H\n", (*p)->data, q->data);
   displayListPN(L);
   displayListPPN(&L);
   
@@ -48,13 +48,13 @@ int main(void) {
 
   updateNodeValue(&L, 'H', 'R');
 
-  deleteAllOccurences(&L, 'R');
+  deleteAllOccurrences(&L, 'R');
 
   insertFirst(&L, 'H');
   insertFirst(&L, 'H');
   insertFirst(&L, 'H');
   insertFirst(&L, 'H');
-  deleteAllOccurences(&L, 'H');
+  deleteAllOccurrences(&L, 'H');
   insertFirst(&L, 'H');
   insertLast(&L, 'R');
 
@@ -71,29 +71,41 @@ int main(void) {
 }
 
 /**
- * @brief Create a new ctype * node with the value input.
+ * @brief Create a new node with the given character value.
  * 
- * @param val 
- * @return List 
+ * @param val The character value for the new node.
+ * @return List A pointer to the newly created node.
  */
 List createNode(char val) {
   List newNode = (ctype *) malloc(sizeof(char));
   newNode->link = NULL;
-  newNode->elem = val;
+  newNode->data = val;
   return newNode;
 }
 
+/**
+ * @brief Insert a new node with the given value at the beginning of the list.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param value The character value to be inserted.
+ */
 void insertFirst(List *L, char value) {
   List newNode = (ctype *)malloc(sizeof(ctype));
-  newNode->elem = value;
+  newNode->data = value;
   newNode->link = *L;
 
   *L = newNode;
 }
 
+/**
+ * @brief Insert a new node with the given value at the end of the list.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param value The character value to be inserted.
+ */
 void insertLast(List *L, char value) {
   List newNode = (List)malloc(sizeof(ctype));
-  newNode->elem = value;
+  newNode->data = value;
   newNode->link = NULL;
 
   List *trav;
@@ -102,35 +114,52 @@ void insertLast(List *L, char value) {
   *trav = newNode;
 }
 
+/**
+ * @brief Insert a new node with the given value in sorted order.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param value The character value to be inserted.
+ */
 void insertSorted(List *L, char value) {
   List *trav;
-  for (trav = L; *trav != NULL && (*trav)->elem < value; trav = &(*trav)->link) {}
+  for (trav = L; *trav != NULL && (*trav)->data < value; trav = &(*trav)->link) {}
 
   List newNode = (ctype *)malloc(sizeof(ctype));
-  newNode->elem = value;
+  newNode->data = value;
   newNode->link = *trav;
   *trav = newNode;
 }
 
+/**
+ * @brief Insert a new node with the given value in sorted order, ensuring uniqueness.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param value The character value to be inserted.
+ */
 void insertSortedUnique(List *L, char value) {
   List *trav;
-  for (trav = L; *trav != NULL && value > (*trav)->elem; trav = &(*trav)->link) {}
+  for (trav = L; *trav != NULL && value > (*trav)->data; trav = &(*trav)->link) {}
 
-  if (*trav == NULL || value != (*trav)->elem) {
+  if (*trav == NULL || value != (*trav)->data) {
     List newNode = (ctype *)malloc(sizeof(ctype));
-    newNode->elem = value;
+    newNode->data = value;
     newNode->link = *trav;
     *trav = newNode;
   }
 }
 
+/**
+ * @brief Delete the node at the specified index.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param targetIndex The index of the node to be deleted.
+ */
 void deleteNodeIndex(List *L, int targetIndex) {
   List *trav = L;
   int currentIndex;
-  // while we don't reach the end of the list & the targetIndex does not match the currentIndex
+
   for (currentIndex = 0; *trav != NULL && currentIndex != targetIndex; trav = &(*trav)->link, currentIndex++) {}
 
-  // if trav does not point to a null address, then we can delete the node it points to
   if (*trav != NULL && currentIndex == targetIndex) {
     List tmp = *trav;
     *trav = tmp->link;
@@ -140,13 +169,17 @@ void deleteNodeIndex(List *L, int targetIndex) {
   }
 }
 
+/**
+ * @brief Delete the first occurrence of a node with the specified value.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param targetValue The value to be deleted.
+ */
 void deleteNodeValue(List *L, char targetValue) {
   List *trav;
 
-  // traverse the list until we find the targetValue in the current node, otherwise reach NULL
-  for (trav = L; *trav != NULL && (*trav)->elem != targetValue; trav = &(*trav)->link) {}
+  for (trav = L; *trav != NULL && (*trav)->data != targetValue; trav = &(*trav)->link) {}
 
-  // if we end early, meaning the 2nd condition: currentElem = targetValue is true
   if (*trav != NULL) {
     List tmp = *trav;
     *trav = tmp->link;
@@ -156,11 +189,17 @@ void deleteNodeValue(List *L, char targetValue) {
   }
 }
 
-void deleteAllOccurences(List *L, char targetValue) {
+/**
+ * @brief Delete all occurrences of nodes with the specified value.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param targetValue The value to be deleted.
+ */
+void deleteAllOccurrences(List *L, char targetValue) {
   List *trav;
 
   for (trav = L; *trav != NULL;) {
-    if ((*trav)->elem == targetValue) {
+    if ((*trav)->data == targetValue) {
       List tmp = *trav;
       *trav = tmp->link;
       free(tmp);
@@ -170,33 +209,55 @@ void deleteAllOccurences(List *L, char targetValue) {
   }
 }
 
+/**
+ * @brief Update the value of the first node with the specified old value.
+ * 
+ * @param L Pointer to the head of the list.
+ * @param oldValue The value to be updated.
+ * @param newValue The new value to be assigned.
+ */
 void updateNodeValue(List *L, char oldValue, char newValue) {
   List *trav;
-  for (trav = L; *trav != NULL && (*trav)->elem != oldValue; trav = &(*trav)->link) {}
+  for (trav = L; *trav != NULL && (*trav)->data != oldValue; trav = &(*trav)->link) {}
 
   if (*trav != NULL) {
-    (*trav)->elem = newValue;
+    (*trav)->data = newValue;
   } else {
     puts("Update Error: There exists no node with that old value!");
   }
 }
 
+/**
+ * @brief Display the elements of the list in the order of pointer notation.
+ * 
+ * @param L The head of the list.
+ */
 void displayListPN(List L) {
   List trav;
   for (trav = L; trav != NULL; trav = trav->link) {
-    printf("[%c]->", trav->elem);
+    printf("[%c]->", trav->data);
   }
   printf("NULL\n");
 }
 
+/**
+ * @brief Display the elements of the list in the order of double pointer notation.
+ * 
+ * @param L Pointer to the head of the list.
+ */
 void displayListPPN(List *L) {
   List *trav;
   for (trav = L; *trav != NULL; trav = &(*trav)->link) {
-    printf("[%c]->", (*trav)->elem);
+    printf("[%c]->", (*trav)->data);
   }
   printf("NULL\n");
 }
 
+/**
+ * @brief Sort the list using the bubble sort algorithm.
+ * 
+ * @param L The head of the list.
+ */
 void bubbleSort(List L) {
   List trav;
   int length = 0, x;
@@ -204,10 +265,10 @@ void bubbleSort(List L) {
   
   for (x = 0; x < length; x++) {
     for (trav = L; trav->link != NULL; trav = trav->link) {
-      if (trav->elem > trav->link->elem) {
-        char tmp = trav->elem;
-        trav->elem = trav->link->elem;
-        trav->link->elem = tmp;
+      if (trav->data > trav->link->data) {
+        char tmp = trav->data;
+        trav->data = trav->link->data;
+        trav->link->data = tmp;
       }
     }
   }
