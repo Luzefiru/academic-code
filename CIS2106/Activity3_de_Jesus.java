038b40a7cc86452331e4844e726eed307b857fc5
Luzefiru <ian.deejay.iv@gmail.com>
2023-11-29 21:30:00 +0800

    feat: finish Activity 4

--- CIS2106/Activity3_de_Jesus.java
+++ CIS2106/Activity3_de_Jesus.java
@@ -1,3 +1,15 @@
+// Declare 3 variables area, length and width of type double.
+// Assign values to length and width to represent the dimensions of the rectangle (you can use any positive numbers).
+// Calculate the area of the rectangle using the formula area = length * width.
+// Display the calculated area on the screen.
+
+// Rectangle Dimensions:
+// Length: 5.5
+// Width: 3.2
+// Area: 17.6
+
+// Your task is to implement this program using variables to store the dimensions and the calculated area of the rectangle. 
+
 public class Activity3_de_Jesus {
   public static void main(String args[]) {
     double length = 5.5;
---
+++ CIS2106/Activity4_de_Jesus.java
@@ -0,0 +1,15 @@
+// Write a Java program to calculate the total cost of a shopping cart. You have three items in the cart, and their prices are 12.50, 5.75, and 9.99, respectively. Calculate and display the total cost of the items.
+
+// Your task is to declare three variables to store the prices of the items and another variable to store the total cost. Use appropriate data types for the variables and perform the necessary arithmetic operations to calculate the total cost.
+
+public class Activity4_de_Jesus {
+  public static void main(String[] args) {
+    double item1 = 12.50;
+    double item2 = 5.75;
+    double item3 = 9.99;
+
+    double totalCost = item1 + item2 + item3;
+
+    System.out.println("Shopping cart total price: " + totalCost);
+  }  
+}
