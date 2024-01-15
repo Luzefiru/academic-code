36c80bb80638a461c0144bd5af8456c267901aec
Luzefiru <ian.deejay.iv@gmail.com>
2024-01-15 20:32:25 +0800

    feat: finish 10.2

--- CIS2106/Activity10_de_Jesus/MultiLevel.java
+++ CIS2106/Activity10_de_Jesus/MultiLevel.java
@@ -1,11 +1,13 @@
 package Activity10_de_Jesus;
 
-public class SingleLevel {
+public class MultiLevel {
   public static void main(String args[]) {
     Christmas ball = new Christmas("This is a ball.");
     System.out.println(ball);
     ChristmasTree tree = new ChristmasTree("Merry Christmas from the de Jesus Family!");
     System.out.println(tree);
+    DecoratedChristmasTree decoratedTree = new DecoratedChristmasTree("Happy New Year!", "Balls, Star, Branches");
+    System.out.println(decoratedTree);
   }
 
   public static class Christmas {
@@ -36,6 +38,20 @@ public class SingleLevel {
     @Override
     public String toString() {
       return super.toString() + String.format("Greeting: %s\n", this.greeting);
+    }
+  }
+
+  public static class DecoratedChristmasTree extends ChristmasTree {
+    String decorations;
+
+    public DecoratedChristmasTree(String greeting, String deco) {
+      super(greeting);
+      this.decorations = deco;
+    }
+
+    @Override
+    public String toString() {
+      return super.toString() + String.format("Decorations: %s\n", this.decorations);
     }
   }
 }
