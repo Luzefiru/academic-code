b6d5ba58de93417839a6492587a04da24ba726fe
Luzefiru <ian.deejay.iv@gmail.com>
2024-01-16 13:29:30 +0800

    fix: refactor 11.2 to use interface

--- CIS2106/Activity11_de_Jesus/Activity11_2.java
+++ CIS2106/Activity11_de_Jesus/Activity11_2.java
@@ -20,42 +20,68 @@ public class Activity11_2 {
     tryndamere.attack(merlin, 20);
   }
 
-  public static abstract class Character {
-    public Integer xPos, yPos, healthPoints, mana;
+  public interface Character {
+    void move(Integer xSteps, Integer ySteps);
 
-    public Character(Integer startingXPos, Integer startingYPos, Integer startingHp, Integer startingMana) {
+    Integer getHealthPoints();
+
+    void setHealthPoints(Integer newHealthPoints);
+
+    Integer getMana();
+
+    void setMana(Integer newMana);
+
+    boolean isDead();
+
+    void attack(Character enemy, Integer damage);
+
+    void useAbility(Character enemy);
+  }
+
+  public static class Warrior implements Character {
+    private final static Integer WARRIOR_STARTING_HP = 20, WARRIOR_STARTING_MANA = 5;
+    private Integer xPos, yPos, healthPoints, mana;
+
+    public Warrior(Integer startingXPos, Integer startingYPos) {
       this.xPos = startingXPos;
       this.yPos = startingYPos;
-      this.healthPoints = startingHp;
-      this.mana = startingMana;
+      this.healthPoints = WARRIOR_STARTING_HP;
+      this.mana = WARRIOR_STARTING_MANA;
     }
 
+    @Override
     public void move(Integer xSteps, Integer ySteps) {
       this.xPos += xSteps;
       this.yPos += ySteps;
       System.out.println(String.format("The new position is (%d ,%d)", this.xPos, this.yPos));
     }
 
+    @Override
     public Integer getHealthPoints() {
       return this.healthPoints;
     }
 
+    @Override
     public void setHealthPoints(Integer newHealthPoints) {
       this.healthPoints = newHealthPoints;
     }
 
+    @Override
     public Integer getMana() {
       return this.mana;
     }
 
+    @Override
     public void setMana(Integer newMana) {
       this.mana = newMana;
     }
 
+    @Override
     public boolean isDead() {
       return this.healthPoints <= 0;
     }
 
+    @Override
     public void attack(Character enemy, Integer damage) {
       enemy.setHealthPoints(enemy.getHealthPoints() - damage);
       if (enemy.isDead()) {
@@ -64,18 +90,6 @@ public class Activity11_2 {
         System.out.println(
             String.format("You did %d damange to the enemy. Their new HP is: %d.", damage, enemy.getHealthPoints()));
       }
-    }
-
-    public void useAbility(Character enemy) {
-      System.out.println("The base character has no available abilities!");
-    };
-  }
-
-  public static class Warrior extends Character {
-    private final static Integer WARRIOR_STARTING_HP = 20, WARRIOR_STARTING_MANA = 5;
-
-    public Warrior(Integer startingXPos, Integer startingYPos) {
-      super(startingXPos, startingYPos, WARRIOR_STARTING_HP, WARRIOR_STARTING_MANA);
     }
 
     @Override
@@ -90,15 +104,62 @@ public class Activity11_2 {
     };
   }
 
-  public static class Mage extends Character {
+  public static class Mage implements Character {
     private final static Integer MAGE_STARTING_HP = 10, MAGE_STARTING_MANA = 30;
+    private Integer xPos, yPos, healthPoints, mana;
 
     public Mage(Integer startingXPos, Integer startingYPos) {
-      super(startingXPos, startingYPos, MAGE_STARTING_HP, MAGE_STARTING_MANA);
+      this.xPos = startingXPos;
+      this.yPos = startingYPos;
+      this.healthPoints = MAGE_STARTING_HP;
+      this.mana = MAGE_STARTING_MANA;
     }
 
     @Override
-    public void useAbility(Character enemy) throws Error {
+    public void move(Integer xSteps, Integer ySteps) {
+      this.xPos += xSteps;
+      this.yPos += ySteps;
+      System.out.println(String.format("The new position is (%d ,%d)", this.xPos, this.yPos));
+    }
+
+    @Override
+    public Integer getHealthPoints() {
+      return this.healthPoints;
+    }
+
+    @Override
+    public void setHealthPoints(Integer newHealthPoints) {
+      this.healthPoints = newHealthPoints;
+    }
+
+    @Override
+    public Integer getMana() {
+      return this.mana;
+    }
+
+    @Override
+    public void setMana(Integer newMana) {
+      this.mana = newMana;
+    }
+
+    @Override
+    public boolean isDead() {
+      return this.healthPoints <= 0;
+    }
+
+    @Override
+    public void attack(Character enemy, Integer damage) {
+      enemy.setHealthPoints(enemy.getHealthPoints() - damage);
+      if (enemy.isDead()) {
+        System.out.println(String.format("You did %d damange to the enemy. They are dead.", damage));
+      } else {
+        System.out.println(
+            String.format("You did %d damange to the enemy. Their new HP is: %d.", damage, enemy.getHealthPoints()));
+      }
+    }
+
+    @Override
+    public void useAbility(Character enemy) {
       if (this.mana >= 10) {
         this.setMana(this.getMana() - 10);
         System.out.println("You cast a fireball on the enemy!");
@@ -106,18 +167,65 @@ public class Activity11_2 {
       } else {
         throw new Error("ERR: Not enough mana to use ability!");
       }
-    };
+    }
   }
 
-  public static class Rogue extends Character {
+  public static class Rogue implements Character {
     private final static Integer ROGUE_STARTING_HP = 15, ROGUE_STARTING_MANA = 20;
+    private Integer xPos, yPos, healthPoints, mana;
 
     public Rogue(Integer startingXPos, Integer startingYPos) {
-      super(startingXPos, startingYPos, ROGUE_STARTING_HP, ROGUE_STARTING_MANA);
+      this.xPos = startingXPos;
+      this.yPos = startingYPos;
+      this.healthPoints = ROGUE_STARTING_HP;
+      this.mana = ROGUE_STARTING_MANA;
     }
 
     @Override
-    public void useAbility(Character enemy) throws Error {
+    public void move(Integer xSteps, Integer ySteps) {
+      this.xPos += xSteps;
+      this.yPos += ySteps;
+      System.out.println(String.format("The new position is (%d ,%d)", this.xPos, this.yPos));
+    }
+
+    @Override
+    public Integer getHealthPoints() {
+      return this.healthPoints;
+    }
+
+    @Override
+    public void setHealthPoints(Integer newHealthPoints) {
+      this.healthPoints = newHealthPoints;
+    }
+
+    @Override
+    public Integer getMana() {
+      return this.mana;
+    }
+
+    @Override
+    public void setMana(Integer newMana) {
+      this.mana = newMana;
+    }
+
+    @Override
+    public boolean isDead() {
+      return this.healthPoints <= 0;
+    }
+
+    @Override
+    public void attack(Character enemy, Integer damage) {
+      enemy.setHealthPoints(enemy.getHealthPoints() - damage);
+      if (enemy.isDead()) {
+        System.out.println(String.format("You did %d damage to the enemy. They are dead.", damage));
+      } else {
+        System.out.println(
+            String.format("You did %d damage to the enemy. Their new HP is: %d.", damage, enemy.getHealthPoints()));
+      }
+    }
+
+    @Override
+    public void useAbility(Character enemy) {
       if (this.mana >= 5) {
         this.setMana(this.getMana() - 5);
         System.out.println("Using your vampiric blades, you stab the enemy!");
@@ -126,6 +234,6 @@ public class Activity11_2 {
       } else {
         throw new Error("ERR: Not enough mana to use ability!");
       }
-    };
+    }
   }
 }
