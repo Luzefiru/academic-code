d1847896ed43722e0b9945c4756c441c4d5f1f8d
Luzefiru <ian.deejay.iv@gmail.com>
2024-10-06 18:41:20 +0800

    fix: merge function

--- CIS3102N/sorting/merge.c
+++ CIS3102N/sorting/merge.c
@@ -20,34 +20,26 @@ void mergeSort(int arr[], int size) {
 
   int middle = size / 2;
 
-  // dynamically allocate memory for the left and right subarrays
-  int *leftArray = malloc(sizeof(int) * middle);
-  int *rightArray = malloc(sizeof(int) * (size - middle));
+  int *leftArray = calloc(middle, sizeof(int));
+  int *rightArray = calloc(size - middle, sizeof(int));
 
-  // copy the left & right sides of the array
   memcpy(leftArray, arr, sizeof(int) * middle);
   memcpy(rightArray, arr + middle, sizeof(int) * (size - middle));
 
-  // recursively break down into smaller subarrays
   mergeSort(leftArray, middle);
   mergeSort(rightArray, size - middle);
 
-  // merge the sorted subarrays back into the original array
   merge(leftArray, middle, rightArray, size - middle, arr);
 
-  // free subarrays since we merged it back to the original already
   free(leftArray);
   free(rightArray);
 }
 
 void merge(int leftArray[], int leftSize, int rightArray[], int rightSize,
            int arr[]) {
-  // to keep track of the leftArray, rightArray, and array - respectively
   int i = 0, j = 0, k = 0;
 
-  // while we still have elements on the left & right arrays
   while (i < leftSize && j < rightSize) {
-    // we put the smallest element from either array into our original array
     if (leftArray[i] < rightArray[j]) {
       arr[k++] = leftArray[i++];
     } else {
@@ -55,12 +47,11 @@ void merge(int leftArray[], int leftSize, int rightArray[], int rightSize,
     }
   }
 
-  // we add back the rest of the elements starting from the left array
   while (i < leftSize) {
     arr[k++] = leftArray[i++];
   }
 
   while (j < rightSize) {
-    arr[k++] = rightArray[i++];
+    arr[k++] = rightArray[j++];
   }
 }
