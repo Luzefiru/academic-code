93f9d99ae3894f4d3bb8642ceef3c99520412721
Luzefiru <ian.deejay.iv@gmail.com>
2024-10-06 21:16:06 +0800

    docs: counting sort comments

--- CIS3102N/sorting/counting.c
+++ CIS3102N/sorting/counting.c
@@ -12,7 +12,7 @@ int main(void) {
 }
 
 void sort(int arr[], int size) {
-  // Find the largest number, k
+  // Get the max value, which will determine the range of the histogram
   int max = arr[0];
   for (int i = 1; i < size; i++) {
     if (arr[i] > max) {
@@ -20,29 +20,22 @@ void sort(int arr[], int size) {
     }
   }
 
-  // Create histogram with k range
+  // Create histogram of numbers of the input array
   int *histogram = calloc(max + 1, sizeof(int));
   for (int i = 0; i < size; i++) {
     histogram[arr[i]]++;
   }
 
-  // Create running sum position array
-  for (int i = 0; i < max + 1; i++) {
-    // if it's the first element, just add the number directly,
-    // otherwise add the previous value as well
-    histogram[i] = i != 0 ? histogram[i - 1] + histogram[i] : histogram[i];
+  // Make prefix sum array
+  for (int i = 1; i < max + 1; i++) {
+    histogram[i] = histogram[i] + histogram[i - 1];
   }
 
-  // Starting from end of input array, assign elements to new output array
+  // Assign to output array, starting from the input array's end via prefix sum
   int *outputArray = calloc(size, sizeof(int));
-
   for (int i = size - 1; i >= 0; i--) {
-    // to get the position of the current index of the input array, we refer to;
-    // the running sum - 1
     outputArray[histogram[arr[i]]-- - 1] = arr[i];
-    // minus 1 since it doesn't account the 0th index
   }
 
-  // Copy the sorted output to the original array
   memcpy(arr, outputArray, sizeof(int) * size);
 }
